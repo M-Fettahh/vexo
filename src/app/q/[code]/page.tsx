2ac800b6b0e995db65fbcc8b8e2f70c466d1e61f
Muhammed Fettah Summak <muhsmmedfettahsummakdeneme@gmail.com>
2026-08-06 12:11:56 +0300

    Son Version

--- src/app/q/[code]/page.tsx
+++ src/app/q/[code]/page.tsx
@@ -1,1 +1,12 @@
+'use client';
 
+import React from 'react';
+import { useParams } from 'next/navigation';
+import App from '../../../App';
+
+export default function QRRoutePage() {
+  const params = useParams();
+  const code = typeof params?.code === 'string' ? params.code : Array.isArray(params?.code) ? params.code[0] : '';
+
+  return <App initialCode={code} />;
+}
